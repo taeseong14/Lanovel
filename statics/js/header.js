27814ec7a2104052986bfcf0a64dc249b0a226d1
taeseong14 <kwontaeseong14@gmail.com>
2022-05-16 17:21:56 -0500

    헤더 다듬을뻔, README와 TODO 분리

--- statics/js/header.js
+++ statics/js/header.js
@@ -5,13 +5,20 @@ header.innerHTML = `
     <nav>
         <ul>
             <li><a href="/">홈</a></li>
-            <li id="search"><input type="search" id="search-input" placeholder="검색"></li>
+            <li id="search"><form id="search-form"><input type="search" id="search-input" placeholder="검색"></form></li>
             <li><a href="/write">쓰</a></li>
             <li><a href="/login">록</a></li>
         </ul>
     </nav>
 `;
 document.body.prepend(header);
+header.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const search = header.querySelector('#search-input');
+    if (!search.value) return;
+    console.log(search.value);
+    search.value = '';
+});
 document.querySelector('header h1').addEventListener('click', () => {
     location.href = '/';
 });
